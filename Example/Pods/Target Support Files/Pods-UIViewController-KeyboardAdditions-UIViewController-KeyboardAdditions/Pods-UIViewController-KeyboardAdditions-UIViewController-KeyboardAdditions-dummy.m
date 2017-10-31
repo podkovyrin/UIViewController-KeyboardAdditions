c159c080e0c4209e0f99c8f8be3f7862e9a761c7
Andrew Podkovyrin <podkovyrin@gmail.com>
2017-10-31 16:43:00 +0300

    Optimize project for Xcode 9

--- Example/Pods/Target Support Files/Pods-UIViewController-KeyboardAdditions-UIViewController-KeyboardAdditions/Pods-UIViewController-KeyboardAdditions-UIViewController-KeyboardAdditions-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_UIViewController_KeyboardAdditions_UIViewController_KeyboardAdditions : NSObject
-@end
-@implementation PodsDummy_Pods_UIViewController_KeyboardAdditions_UIViewController_KeyboardAdditions
-@end
